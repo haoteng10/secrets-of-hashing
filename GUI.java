b3a87ae2da60358d53fe3dc24a5d5dd3b80b5d51
Hao Teng <haoteng10@gmail.com>
2022-12-15 22:13:53 -0500

    feat: tests for GUI.java

--- GUI.java
+++ GUI.java
@@ -28,8 +28,8 @@ public class GUI {
     private static JTextArea inputArea; // Java Swing's Text Area for Input
     private static JTextArea outputArea; // Java Swing's Text Area for Output
 
-    private static final String placeholderMessage = "Write your message here"; // Default Message
-    
+    private static final String PLACEHOLDER_MESSAGE = "Write your message here"; // Default Message
+
     // Renders the graphical user interface
     public GUI() {
 
@@ -48,19 +48,19 @@ public class GUI {
         // Input Text Area
         // Adapted from Oracle Documentation
         // https://docs.oracle.com/javase/tutorial/uiswing/components/textarea.html
-        inputArea = new JTextArea(placeholderMessage);
+        inputArea = new JTextArea(PLACEHOLDER_MESSAGE);
         inputArea.setLineWrap(true);
         inputArea.setWrapStyleWord(true);
         inputArea.addFocusListener(new FocusListener() {
 
             public void focusGained(FocusEvent e) {
-                if (inputArea.getText().equals(placeholderMessage))
+                if (inputArea.getText().equals(PLACEHOLDER_MESSAGE))
                     inputArea.setText("");
             }
 
             public void focusLost(FocusEvent e) {
                 if (inputArea.getText().isEmpty())
-                    inputArea.setText(placeholderMessage);
+                    inputArea.setText(PLACEHOLDER_MESSAGE);
             }
         });
 
@@ -145,15 +145,38 @@ public class GUI {
         setStatus("");
 
         // Clear textAreas
-        inputArea.setText(placeholderMessage);
+        inputArea.setText(PLACEHOLDER_MESSAGE);
         outputArea.setText("");
     }
 
     // Main method to run
     public static void main(String[] args) {
-        // Testing prior to running
-        // Check status method
 
+        // Renders the graphical user interface
         new GUI();
+
+        // Making sure all Swing components are initialized
+        if (statusLabel == null) StdOut.println("Status Label not initialized.");
+        if (inputArea == null) StdOut.println("Input TextArea not initialized.");
+        if (outputArea == null) StdOut.println("Output TextArea not intialized.");
+
+        // Making sure all setters will manipulate correct instance variables
+        setSelectedFile(new File("test.txt"));
+        if (selectedFile == null) StdOut.println(
+                "Setter for selectedFile not modifying the correct instance variable.");
+
+        // Making sure all getters return correct values
+        if (selectedFile != getSelectedFile()) StdOut.println(
+                "Getter for selectedFile not returning correct value.");
+
+        // Making sure setStatus works
+        String TEST_STRING = "test 123";
+        setStatus(TEST_STRING);
+        if (!statusLabel.getText().equals(TEST_STRING))
+            StdOut.println("Not setting the status correctly");
+
+        // Making sure the clear function works
+        clear();
+        if (getSelectedFile() != null) StdOut.println("Error in clearing the inputs");
     }
 }
